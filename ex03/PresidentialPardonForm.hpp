44374a02350d79dfab30b2888c30446d2ab6fd6a
Samuele Dell'Erba <sdell-er@c2r1p6.42firenze.it>
2024-12-20 19:55:24 +0100

    fine

--- ex03/PresidentialPardonForm.hpp
+++ ex03/PresidentialPardonForm.hpp
@@ -1,34 +1,32 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ShrubberyCreationForm.hpp                          :+:      :+:    :+:   */
+/*   PresidentialPardonForm.hpp                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: sdell-er <sdell-er@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/12/18 18:00:14 by sdell-er          #+#    #+#             */
-/*   Updated: 2024/12/20 17:08:55 by sdell-er         ###   ########.fr       */
+/*   Created: 2024/12/20 16:40:33 by sdell-er          #+#    #+#             */
+/*   Updated: 2024/12/20 16:41:53 by sdell-er         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef SHRUBBERYCREATIONFORM_HPP
-# define SHRUBBERYCREATIONFORM_HPP
+#ifndef PRESIDENTIALPARDONFORM_HPP
+# define PRESIDENTIALPARDONFORM_HPP
 
 # include "AForm.hpp"
-# include <fstream>
 
-class ShrubberyCreationForm : public AForm
+class PresidentialPardonForm : public AForm
 {
 	private:
 		std::string _target;
-		std::string createTrees() const;
 
 	public:
-		ShrubberyCreationForm();
-		ShrubberyCreationForm(std::string target);
-		ShrubberyCreationForm(const ShrubberyCreationForm &copy);
-		~ShrubberyCreationForm();
-		ShrubberyCreationForm &operator=(const ShrubberyCreationForm &copy);
- 
+		PresidentialPardonForm();
+		PresidentialPardonForm(std::string target);
+		PresidentialPardonForm(const PresidentialPardonForm &copy);
+		~PresidentialPardonForm();
+		PresidentialPardonForm &operator=(const PresidentialPardonForm &copy);
+
 		virtual void execution() const;
 };
 
--- ex03/RobotomyRequestForm.hpp
+++ ex03/RobotomyRequestForm.hpp
@@ -1,33 +1,32 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ShrubberyCreationForm.hpp                          :+:      :+:    :+:   */
+/*   RobotomyRequestForm.hpp                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: sdell-er <sdell-er@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/12/18 18:00:14 by sdell-er          #+#    #+#             */
-/*   Updated: 2024/12/20 17:08:55 by sdell-er         ###   ########.fr       */
+/*   Created: 2024/12/20 16:38:38 by sdell-er          #+#    #+#             */
+/*   Updated: 2024/12/20 17:40:43 by sdell-er         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef SHRUBBERYCREATIONFORM_HPP
-# define SHRUBBERYCREATIONFORM_HPP
+#ifndef ROBOTOMYREQUESTFORM_HPP
+# define ROBOTOMYREQUESTFORM_HPP
 
 # include "AForm.hpp"
-# include <fstream>
+# include <cstdlib>
 
-class ShrubberyCreationForm : public AForm
+class RobotomyRequestForm : public AForm
 {
 	private:
 		std::string _target;
-		std::string createTrees() const;
 
 	public:
-		ShrubberyCreationForm();
-		ShrubberyCreationForm(std::string target);
-		ShrubberyCreationForm(const ShrubberyCreationForm &copy);
-		~ShrubberyCreationForm();
-		ShrubberyCreationForm &operator=(const ShrubberyCreationForm &copy);
+		RobotomyRequestForm();
+		RobotomyRequestForm(std::string target);
+		RobotomyRequestForm(const RobotomyRequestForm &copy);
+		~RobotomyRequestForm();
+		RobotomyRequestForm &operator=(const RobotomyRequestForm &copy);
  
 		virtual void execution() const;
 };
